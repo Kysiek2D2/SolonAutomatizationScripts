6c415e04865a89dd118330aa14bc44dd9503cf5d
(KRDA) Krzysztof Dabkowski <krda@netcompany.com>
2025-11-21 15:46:17 +0100

    Update

--- src/generator.py
+++ src/generator.py
@@ -12,57 +12,58 @@ import csv
 TEMPLATE_CODELIST = """
         <!--   codelist     -->
         <insert tableName="codelist">
-            <column name="codelist_key" value="LT_TYPES_OF_PERSONS"/>
+            <column name="codelist_key" value="XXXXXXXXXXXXXXXXXXXXXXXXX"/>
             <column name="codelist_type"/>
             <column name="owner_cl" value="CUSTOM"/>
         </insert>
 
         <insert tableName="codelist_label">
-            <column name="description" value="Person types" />
-            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = 'LT_TYPES_OF_PERSONS')" />
+            <column name="description" value=""XXXXXXXXXXXXXXXXXXXXXXXXX"" />
+            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = '"XXXXXXXXXXXXXXXXXXXXXXXXX"')" />
             <column name="language_cl" value="EN" />
         </insert>
 
         <insert tableName="codelist_label">
-            <column name="description" value="Asmenų tipai" />
-            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = 'LT_TYPES_OF_PERSONS')" />
+            <column name="description" value=""XXXXXXXXXXXXXXXXXXXXXXXXX"" />
+            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = '"XXXXXXXXXXXXXXXXXXXXXXXXX"')" />
             <column name="language_cl" value="LT" />
         </insert>
 
         <insert tableName="codelist_category">
-            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = 'LT_TYPES_OF_PERSONS')" />
-            <column name="category_code" value="REGISTRATION" />
+            <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = '"XXXXXXXXXXXXXXXXXXXXXXXXX"')" />
+            <column name="category_code" value=""XXXXXXXXXXXXXXXXXXXXXXXXX"" />
         </insert>
 """
 
 TEMPLATE_ITEM = """
-        <!--   {{ID_number}} --> 
+        <!--   {{item_code}} --> 
         <insert tableName="codelist_item">
-            <column name="item_code" value="{{ID_number}}"/>
+            <column name="item_code" value="{{item_code}}"/>
             <column name="codelist_id" valueComputed="(select id from codelist where codelist_key = '{{codelist}}')"/> 
             <column name="owner_cl" value="CUSTOM"/>
             <column name="valid_from" value="1900-01-01 01:00:00.000 +0200"/>
             <column name="valid_to" valueComputed="NULL"/>
+            <column name="sorting" value="{{sorting}}"/>
         </insert>
 
         <insert tableName="codelist_item_label">
             <column name="item_id" valueComputed="(
                 select ci.id from codelist_item ci
                 join codelist c on ci.codelist_id = c.id
-                where ci.item_code = '{{ID_number}}' and c.codelist_key = '{{codelist}}'
+                where ci.item_code = '{{item_code}}' and c.codelist_key = '{{codelist}}'
             )"/>
             <column name="language_cl" value="EN"/>
-            <column name="description" value="{{EN_translation}}"/> 
+            <column name="description" value="{{EN_description}}"/> 
         </insert>
 
         <insert tableName="codelist_item_label">
             <column name="item_id" valueComputed="(
                 select ci.id from codelist_item ci
                 join codelist c on ci.codelist_id = c.id
-                where ci.item_code = '{{ID_number}}' and c.codelist_key = '{{codelist}}'
+                where ci.item_code = '{{item_code}}' and c.codelist_key = '{{codelist}}'
             )"/>
             <column name="language_cl" value="LT"/>
-            <column name="description" value="{{LT_translation}}"/> 
+            <column name="description" value="{{LT_description}}"/> 
         </insert>
 """
 
@@ -82,10 +83,11 @@ with open(CSV_PATH, encoding="utf-8") as file:
     reader.fieldnames = [name.strip().replace('\ufeff','') for name in reader.fieldnames]  #\ufeff is some weird sequence in case you have utf-8-sig file
     for row in reader:
         current_row = TEMPLATE_ITEM
-        current_row = current_row.replace("{{ID_number}}", row["ID_number"])
+        current_row = current_row.replace("{{item_code}}", row["item_code"])
         current_row = current_row.replace("{{codelist}}", row["codelist"])
-        current_row = current_row.replace("{{EN_translation}}", row["EN_translation"])
-        current_row = current_row.replace("{{LT_translation}}", row["LT_translation"])
+        current_row = current_row.replace("{{EN_description}}", row["EN_description"])
+        current_row = current_row.replace("{{LT_description}}", row["LT_description"])
+        current_row = current_row.replace("{{sorting}}", row["sorting"])
 
         final_script += current_row + "\n"
 
